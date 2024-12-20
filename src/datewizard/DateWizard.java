c020c19842ffb7fb77570477dc69047a45501a0f
Arka Sahoo <arkasahoo2005@gmail.com>
2024-12-20 16:38:33 -0600

    Test change

--- src/datewizard/DateWizard.java
+++ src/datewizard/DateWizard.java
@@ -1,7 +1,9 @@
 package datewizard;
 
 public class DateWizard {
-
+    public DateWizard() {
+        System.out.println("Hello");
+    }
 }
 
 // Make an object. Constructor will take a string. Every functionality will be a singular method.
